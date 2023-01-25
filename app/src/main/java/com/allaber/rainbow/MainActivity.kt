97e4c380b68d5250cd9523a418d0b73ca2ba6cbf
allaberx <allaberdinx@gmail.com>
2023-01-26 01:44:13 +0500

    added theme switcher

--- app/src/main/java/com/allaber/rainbow/MainActivity.kt
+++ app/src/main/java/com/allaber/rainbow/MainActivity.kt
@@ -2,10 +2,31 @@ package com.allaber.rainbow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.allaber.rainbow.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding: ActivityMainBinding by viewBinding(
+        ActivityMainBinding::bind
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.switchTheme.setOnCheckedChangeListener{ _: CompoundButton, theme: Boolean ->
+            selectTheme(theme)
+        }
+    }
+
+    private fun selectTheme(theme: Boolean) {
+        if(theme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
